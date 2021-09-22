1f5f179264902a6a839d8e8f0482428dfba2c710
uzuzer567 <49733302+uzuzer567@users.noreply.github.com>
2021-09-22 13:14:24 +0300

    feat: add meet block slider

--- pages/main/main.js
+++ pages/main/main.js
@@ -3,3 +3,18 @@ document.querySelector('.header__burger').onclick = function () {
   	document.querySelector('.header__nav').classList.toggle('active');
 	document.querySelector('body').classList.toggle('lock');
 }
+
+const sliderBlock = document.querySelector('.meet-block__info');
+const sliderItems = document.querySelector('.meet-block__slider-cards');
+
+(function() {
+	sliderBlock.addEventListener('click', addCloneFirstLast);
+	
+	function addCloneFirstLast(e) {
+	  let first = sliderItems.querySelector('.meet-block__card');
+	  let last = sliderItems.querySelector('.meet-block__card:last-child');
+
+	  if (e.target.classList.contains('button-left')) sliderItems.insertBefore(last, first);
+	  if (e.target.classList.contains('button-right')) sliderItems.appendChild(first);
+	};
+}).call(this);
